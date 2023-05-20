4a1c7ec0fdeb47d65202c13eb839a29f0a8f503d
Nadiia Tsytsylina <miniova95@gmail.com>
2023-05-20 20:36:19 +0300

    upd transactions styles

--- src/components/transactionHistory/TransactionHistory.js
+++ src/components/transactionHistory/TransactionHistory.js
@@ -1,22 +1,23 @@
 import PropTypes from 'prop-types';
+import css from 'components/TransactionHistory/TransactionHistory.module.css';
 
 export default function TransactionHistory({ items }) {
   return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+    <table className={css.transactionHistory}>
+      <thead className={css.thead}>
+        <tr className={css.Headrow}>
+          <th className={css.headColumn}>Type</th>
+          <th className={css.headColumn}>Amount</th>
+          <th className={css.headColumn}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map(item => (
-          <tr key={item.id}>
-            <td>{item.type}</td>
-            <td>{item.amount}</td>
-            <td>{item.currency}</td>
+          <tr key={item.id} className={css.row}>
+            <td className={css.column}>{item.type}</td>
+            <td className={css.column}>{item.amount}</td>
+            <td className={css.column}>{item.currency}</td>
           </tr>
         ))}
       </tbody>
