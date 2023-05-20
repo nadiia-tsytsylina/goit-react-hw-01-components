f07921fe78ea0be1161144e3302608ff0114d995
Nadiia Tsytsylina <miniova95@gmail.com>
2023-05-20 21:17:17 +0300

    Update FriendListItem.js

--- src/components/friendListItem/FriendListItem.js
+++ src/components/friendListItem/FriendListItem.js
@@ -4,7 +4,7 @@ import css from './FriendListItem.module.css';
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <li className={css.item}>
-      <span classNames={css.status}>{isOnline ? 'online' : 'offline'}</span>
+      <span className={css.status}>{isOnline ? 'online' : 'offline'}</span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
