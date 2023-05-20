d6ea779fe0084f8ae13fecd2e5b18af8cd7c0ab7
Nadiia Tsytsylina <miniova95@gmail.com>
2023-05-20 21:07:43 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import user from 'json/user.json';
 import data from 'json/data.json';
 import friends from 'json/friends.json';
 import transactions from 'json/transactions.json';
-import Profile from './components/Profile/Profile';
-import Statistics from './components/Statistics/Statistics';
-import FriendList from './components/FriendList/FriendList';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import Profile from 'components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
+import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
 
 export default function App() {
   return (
