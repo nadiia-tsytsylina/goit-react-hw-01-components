3e11c023c85fb414411ef1012b6af1f6cb211b0a
Nadiia Tsytsylina <miniova95@gmail.com>
2023-05-20 20:35:36 +0300

    upd comon styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import user from './json/user.json';
-import data from './json/data.json';
-import friends from './json/friends.json';
-import transactions from './json/transactions.json';
-import Profile from 'components/profile/Profile';
-import Statistics from 'components/statistics/Statistics';
-import FriendList from 'components/friendList/FriendList';
-import TransactionHistory from 'components/transactionHistory/TransactionHistory';
+import user from 'json/user.json';
+import data from 'json/data.json';
+import friends from 'json/friends.json';
+import transactions from 'json/transactions.json';
+import Profile from 'components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
+import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
 
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Profile
         avatar={user.avatar}
         userName={user.username}
